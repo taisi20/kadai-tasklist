86a40354578394ea28b58e7eeeb4db0b3d0d1f5d
taisi20 <tokudotai@outlook.jp>
2019-05-15 12:22:54 +0000

    マイグレーションファイルの修正

--- db/migrate/20190515032527_change_column_to_tasks.rb
+++ db/migrate/20190515032527_change_column_to_tasks.rb
@@ -1,7 +1,4 @@
 class ChangeColumnToTasks < ActiveRecord::Migration[5.2]
   def change
-    remove_foreign_key :tasks, :users
-    remove_index :tasks, :user_id
-    remove_reference :tasks, :user
   end
 end
